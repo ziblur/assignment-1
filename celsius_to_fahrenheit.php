d9296718c2a5f932780ffbcbe2fab8ede13e80c2
ziblur <ziblur@gmail.com>
2024-07-04 20:49:02 +0600

    first commit

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,5 @@
+<?php
+$celsius = 32;
+$farenheit = (9/5*$celsius)+32;
+printf("%d deg celcius is equal to %.2F deg farenheit",$celsius,$farenheit);
+?>
---
+++ largest_number.php
@@ -0,0 +1,21 @@
+<?php
+    $num1 = 4;
+    $num2 = 5;
+    $num3 = 6;
+    if($num1>$num2){
+        if($num1>$num3){
+            print("num1 is the largest number");
+        }
+        else{
+            print("num3 is the largest number");
+        }
+    }
+    else{
+        if($num2>$num3){
+            print("num2 is the largest number");
+        }
+        else{
+            print("num3 is the largest number");
+        }
+    }
+?>
